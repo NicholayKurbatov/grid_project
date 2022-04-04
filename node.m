5f641a3f725afc4b5450bfe4b73a9e596d5ad862
Nicholay Kurbatov <nicholay.kurbatov@yandex.ru>
2022-04-04 16:15:59 +0300

    Update node

--- node.m
+++ node.m
@@ -3,31 +3,55 @@ classdef node < handle
     %   Detailed explanation goes here
     
     properties
-        id
-        line_in % родительская линия
-        line_out % дочерняя линия
-        I
-        U
-        sigma
-        load
+        id 
+        line_in % Parent's line (указатели)
+        line_out % Children's line
+        I % Vectot current
+        U % Vectot voltage
+        sigma % Matrix conductive 
+        load % Matrix load
         
     end
     
     methods
-        % конструктор
-        function obj = node(id, line_in, line_out, I, U, sigma, load)
+        % Constucter
+        function obj = node(id, varargin)
             %==============================================================
             % id -- integer 
-            % line_in --
-            % I, U
+            % line_in, line_out -- 
+            % I, U -- Vector (3,1)
+            % sigma -- Matrix (3,3)
+            % load -- Matrix (3,3)
             %==============================================================
             obj.id = id;
-            obj.line_in = line_in; 
-            obj.line_out = line_out; 
-            obj.I = I;
-            obj.U = U;
-            obj.sigma = sigma;
-            obj.load = load;
+            if (obj(varargin) <= 1) && ~isempty(obj(varargin{1}))
+                obj.line_in = obj(varargin{1}); 
+            end
+
+            if (obj(varargin) <= 1) && ~isempty(obj(varargin{2}))
+                obj.line_out = obj(varargin{2}); 
+            end
+
+            if (obj(varargin) <= 1) && ~isempty(obj(varargin{4}))
+                obj.I = obj(varargin{4}); 
+            end
+
+            if (obj(varargin) <= 1) && ~isempty(obj(varargin{5}))
+                obj.I = obj(varargin{5}); 
+            end
+
+            if (obj(varargin) <= 1) && ~isempty(obj(varargin{6}))
+                obj.U = obj(varargin{6}); 
+            end
+            
+            if (obj(varargin) <= 1) && ~isempty(obj(varargin{7}))
+                obj.sigma = obj(varargin{7}); 
+            end
+ 
+            if (obj(varargin) <= 1) && ~isempty(obj(varargin{8}))
+                obj.load = obj(varargin{8}); 
+            end
+
 
         end
 
