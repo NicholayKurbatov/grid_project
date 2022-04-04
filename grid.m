72247437080446995ccaffaef350aca3d8ab0db5
Курбатов Николай Витальевич <st064324@student.spbu.ru>
2022-04-04 15:39:39 +0300

    add varargin

--- grid.m
+++ grid.m
@@ -3,25 +3,34 @@ classdef grid < handle
     %   Detailed explanation goes here
     
     properties
-        eds_src
-        z_src
-        src_id
+        eds_src = ones(3, 1)
+        z_src = ones(3, 3)
+        src_id = 1
         node_list
         line_list
     end
     
     methods
         % конструктор
-        function obj = grid(eds_src, z_src, src_id)
+        function obj = grid(src_id, varargin)
             % ===================================================
+            % src_id  -- str, source node id 
             % eds_src -- float array (3, 1)
             % z_src   -- complex array (3, 3)
-            % src_id  -- str, source node id 
             % ===================================================
             
-            % задаем параметры источника
-            obj.eds_src = eds_src;
-            obj.z_src = z_src;
+            % задаем параметры источника, если нужно
+            if (numel(varargin) >= 1) && ~isempty(varargin{1})
+               % проверка на размерность
+               obj.eds_src = eds_src;
+               if ~isempty(varargin{2})
+                   % проверка на размерность
+                   obj.z_src = z_src;
+               end
+            else
+                obj.eds_src = eds_src;
+                obj.z_src = z_src;
+            end
             
             % созадем узел, который соответсвует источнику
             add_node(obj, obj.src_id)
@@ -31,16 +40,17 @@ classdef grid < handle
         
         
         % добавление узла
-        function add_node(this, node_name)
+        function add_node(this, node_id, varargin)
             % ===================================================
             % node_name -- str, unique node name 
+            % 
             % ===================================================
             
         end
         
         
         % добавление линии
-        function add_line(this, line_name, node_in, node_out) 
+        function add_line(this, line_id, node_in_id, node_out_id, varargin) 
             % ===================================================
             % line_name -- str, unique line name 
             % node_in   -- str, unique node name in 
